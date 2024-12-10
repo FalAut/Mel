8896ca86184abec8f8577eca634857a1db42ff9e
FalAut <2907281799@qq.com>
2024-12-10 18:51:55 +0000

    为版本 0.1.4 添加自动更新补丁

---
+++ updater/hotfixes/0.1.4/kubejs/server_scripts/common/init.js
@@ -0,0 +1,39 @@
+// priority: 9
+
+ServerEvents.loaded((event) => {
+    const { server } = event;
+
+    if (!server.persistentData.getBoolean("first_load")) {
+        server.gameRules.set("keepInventory", "true");
+        server.gameRules.set("doInsomnia", "false");
+        server.gameRules.set("doTraderSpawning", "false");
+        server.gameRules.set("mobGriefing", "false");
+        server.gameRules.set("doWeatherCycle", "false");
+        server.gameRules.set("doDaylightCycle", "false");
+
+        server.getLevel("minecraft:overworld").setDayTime(6000);
+        server.persistentData.putBoolean("first_load", true);
+    }
+});
+
+// ServerEvents.loaded((event) => {
+//     let fuelItems = {};
+
+//     Ingredient.all.stacks.forEach((itemStack) => {
+//         const burnTime = $ForgeHooks.getBurnTime(itemStack, "minecraft:smelting");
+
+//         if (burnTime > 0) {
+//             fuelItems[itemStack.id] = burnTime;
+//         }
+//     });
+
+//     JsonIO.write("kubejs/fuel_items.json", fuelItems);
+// });
+
+// ItemEvents.rightClicked("stick", (event) => {
+//     let allItems = Ingredient.all.stacks.toArray();
+//     let randomIndex = Utils.random.nextInt(allItems.length);
+//     let randomItem = allItems[randomIndex];
+
+//     event.player.give(randomItem);
+// });
