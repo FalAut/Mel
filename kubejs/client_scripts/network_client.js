49f5a3d478bd5b73d316604f9d69b9b0a8c38ea9
FalAut <143469089+FalAut@users.noreply.github.com>
2024-12-11 00:38:16 +0800

    vkk

--- kubejs/client_scripts/network_client.js
+++ kubejs/client_scripts/network_client.js
@@ -23,7 +23,6 @@ ClientEvents.tick((event) => {
     }
 });
 
-// test
 // clear void
 ClientEvents.loggedIn((event) => {
     let isFlatField = $ClientLevelData.__javaObject__.getDeclaredField("f_104832_");
